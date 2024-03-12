7fc421f6a6d9f0f54eee29658bcca8919b827da1
chaitanya <saichaitanya000786@gmail.com>
2024-03-13 00:39:35 +0530

    Chaitanya_13032024

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 // App.js
 import React, { useState } from 'react';
 import WeatherService from './WeatherService';
+import './App.css';
 
 function App() {
   const [cityName, setCityName] = useState('');
@@ -52,26 +53,40 @@ function App() {
 
   return (
     <div>
-      <h1>Weather Forecast</h1>
-      <input
-        type="text"
-        placeholder="Enter city name"
-        value={cityName}
-        onChange={(event) => setCityName(event.target.value)}
-      />
-      <button onClick={fetchWeatherData}>Get Weather Data</button>
+      <div class="mainDiv">
+        <div class="childDiv"><h1>Weather in your city</h1></div>
+      <div class="childDiv">
+          <input
+            type="text"
+            placeholder="Enter city name"
+            value={cityName}
+            onChange={(event) => setCityName(event.target.value)}
+          />
+          <button onClick={fetchWeatherData}>? Search</button>
+          
+      </div>
+      </div>
       {error && <div>{error}</div>}
       {weatherData && (
-        <div>
+        <div class="dataDiv">
           {weatherData.map((dayData, index) => (
-            <div key={index}>
-              <h2>Date: {new Date(dayData[0].dt_txt).toLocaleDateString()}</h2>
-              <p>
-                Min Temperature: {dayData[0].main.temp_min}°C, Max Temperature: {dayData[0].main.temp_max}°C
-              </p>
-              <p>Pressure: {dayData[0].main.pressure}</p>
-              <p>Humidity: {dayData[0].main.humidity}</p>
-            </div>
+            <table key={index}>
+              <tr><th colspan="2" class="dateCol">Date: {new Date(dayData[0].dt_txt).toLocaleDateString()}</th></tr>
+              <tr><th colspan="2" class="subData">Temperature</th></tr>
+              <tr class="subData"><td>Min</td><td>Max</td></tr> 
+              <tr class="subData"> 
+                  <td>{dayData[0].main.temp_min}°C</td> 
+                  <td>{dayData[0].main.temp_max}°C</td> 
+              </tr> 
+              <tr> 
+                  <td>Pressure</td> 
+                  <td>{dayData[0].main.pressure}</td> 
+              </tr> 
+              <tr> 
+                  <td>Humidity</td> 
+                  <td> {dayData[0].main.humidity}</td> 
+              </tr> 
+            </table>
           ))}
         </div>
       )}
